4897c31fb532b348d9ab600d47ad437665224dd5
宋欢 <trotri@yeah.net>
2014-11-09 02:08:19 +0800

    测试完成

---
+++ app/site/views/bootstrap/components/menus/navbar/default.php
@@ -0,0 +1,9 @@
+<!-- NavBar -->
+<div class="blog-masthead">
+  <div class="container">
+    <ul class="nav navbar-nav blog-nav">
+<?php echo $this->menus; ?>
+    </ul>
+  </div>
+</div>
+<!-- /NavBar -->
